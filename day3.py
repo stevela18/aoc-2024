89d05671938c74d80837aadc3990e22fd6c86302
JonatanBenarroch <>
2024-12-03 13:35:13 +0000

    day3

---
+++ day3.py
@@ -0,0 +1,62 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3.12
+#     language: python
+#     name: py312
+# ---
+
+# %%
+import pandas as pd
+import numpy as np
+import regex as re
+
+# %%
+with open("day3input.txt", "r") as file:
+    corrupted_string = file.read()
+
+
+# %%
+def mul(s):
+    numbers_string = re.findall(r"\d{1,3}", s)
+    numbers = [int(x) for x in numbers_string]
+    product = 1
+    for n in numbers:
+        product = product*n
+    return product
+
+
+# %%
+pattern = r"mul\(\d{1,3},\d{1,3}\)"
+
+
+# %%
+def sum_of_products(input_s):
+    total = 0
+    for i in re.finditer(pattern, input_s):
+        s = i.group()
+        total += mul(s)
+    return total
+# sum of all mult commands
+print(sum_of_products(corrupted_string))
+
+# %%
+test = corrupted_string[:2000]
+print(test)
+
+# %%
+sum_dos = 0
+all_the_dos = re.split(r"do(?!n't)", corrupted_string)
+for do in all_the_dos:
+    dont_in_dos = re.split(r"don't", do)
+    sum_dos += sum_of_products(dont_in_dos[0])
+# sum of all the mult commands which follow a do before they follow a don't
+print(sum_dos)
+
+# %%
