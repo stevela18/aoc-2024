4bec7caa45f7d4c1c0fb1538c803eb98db5470da
JonatanBenarroch <>
2024-12-05 10:09:34 +0000

    files

---
+++ day5.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3.12
+#     language: python
+#     name: py312
+# ---
+
+# %%
+import numpy as np
+
+# %%
