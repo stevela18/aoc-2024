3c3fcfce618b41ed85f95f89481e080e25a1957c
JonatanBenarroch <>
2024-12-09 23:20:55 +0000

    day 9 incomplete

--- day9.py
+++ day9.py
@@ -5,11 +5,11 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.16.2
+#       jupytext_version: 1.16.4
 #   kernelspec:
-#     display_name: Python 3.11
+#     display_name: Python 3.12
 #     language: python
-#     name: py311
+#     name: py312
 # ---
 
 # %%
@@ -95,5 +95,75 @@ s_list = [-1 if x=="." else int(x) for x in s_list ]
 
 # %%
 check_sum(s_list)
+
+# %%
+file_system = create_file_system(example)
+
+# %%
+print(file_system)
+
+
+# %%
+def file_system_intervals(s):
+    filled_intervals = []
+    empties_intervals = []
+    # file_number = 0
+    count = 0
+    for i, c in enumerate(s):
+        new_count = count + int(c)
+        if i%2==0:
+            filled_intervals.append((count, new_count))
+            # file_number += 1
+        elif int(c):
+            empties_intervals.append((count, new_count))
+        count = new_count
+    return filled_intervals, empties_intervals
+
+
+# %%
+file_system = create_file_system(example)
+filled_intervals, empties_intervals = file_system_intervals(example)
+
+
+# %%
+def compress2(s):
+    compressed = create_file_system(s)
+    filled_intervals, empties_intervals = file_system_intervals(s)
+    for i in range(len(filled_intervals)-1, -1, -1):
+        f = filled_intervals[i]
+        len_f = f[1] - f[0]
+        flag = True
+        j = 0
+        while (j<len(empties_intervals))&(flag):
+            e = empties_intervals[j]
+            len_e = e[1] - e[0]
+            if len_e >= len_f:
+                compressed[e[0]:(e[0]+len_f)] = [i]*len_f
+                if len_e == len_f:
+                    empties_intervals.remove(e)
+                else:
+                    empties_intervals[j] = (e[0]+len_f, e[1])
+                compressed[f[0]:f[1]] = [-1]*len_f
+                while compressed[-1] < 0:
+                    compressed = compressed[:-1]
+                flag = False
+            j+=1
+    return compressed
+
+
+# %%
+compressed = compress2(example)
+
+# %%
+print(compressed)
+
+# %%
+print(compressed)
+
+# %%
+check_sum(compress2(example))
+
+# %%
+print(check_sum(compress2(input_string)))
 
 # %%
